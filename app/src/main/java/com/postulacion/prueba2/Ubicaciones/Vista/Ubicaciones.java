80fcf26d41f9e3a427d6d55d973e7ffcf2cf9221
abimael.perez <abimael.perez@ennovasoft.com>
2020-12-14 20:40:02 -0600

    0.0.2 - Bisman Abimael Perez Lemus - Se agrego consumo de imagenes

--- app/src/main/java/com/postulacion/prueba2/Ubicaciones/Vista/Ubicaciones.java
+++ app/src/main/java/com/postulacion/prueba2/Ubicaciones/Vista/Ubicaciones.java
@@ -1,12 +1,11 @@
 package com.postulacion.prueba2.Ubicaciones.Vista;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.postulacion.prueba2.R;
 
